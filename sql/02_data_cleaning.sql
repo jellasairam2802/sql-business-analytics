09705032639a0f4b9d66f956549f46b9c8b2ed16
jellasairam2802 <jellasairam2802@gmail.com>
2026-06-24 02:02:49 -0400

    Update 02_data_cleaning.sql

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -1,1 +1,37 @@
+USE dc_violations;
 
+-- Check row counts
+
+SELECT COUNT(*) AS violations_count
+FROM violations;
+
+SELECT COUNT(*) AS weather_count
+FROM weather;
+
+-- Check for NULL values
+
+SELECT *
+FROM violations
+WHERE ticket_id IS NULL;
+
+SELECT *
+FROM weather
+WHERE weather_date IS NULL;
+
+-- Remove duplicate violation records
+
+SELECT ticket_id,
+       COUNT(*) AS duplicate_count
+FROM violations
+GROUP BY ticket_id
+HAVING COUNT(*) > 1;
+
+-- Verify date formats
+
+SELECT DISTINCT weather_date
+FROM violations
+LIMIT 20;
+
+SELECT DISTINCT weather_date
+FROM weather
+LIMIT 20;
